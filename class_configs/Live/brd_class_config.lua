6d132d26997cc264d3761dcb2567e34b4a91283c
Xatrekak <dustin.lee@arista.com>
2026-05-25 02:35:35 -0400

    Add missing LDoN Aria and added EmergencyStart check to combat fade to make it a little less aggresive

--- class_configs/Live/brd_class_config.lua
+++ class_configs/Live/brd_class_config.lua
@@ -149,6 +149,7 @@ local _ClassConfig = {
             "Eriki's Psalm of Power",    -- level 69, OoW
             "Yelhun's Mystic Call",      -- level 68, OoW
             "Echo of the Trusik",        -- level 65, GoD
+            "Call of the Muse",          -- level 65, LDoN
             "Rizlona's Call of Flame",   -- level 64, PoP (overhaste/spell damage)
         },
         ['OverhasteSong'] = {            -- before effects are combined in aria
@@ -1412,7 +1413,7 @@ local _ClassConfig = {
                 type = "AA",
                 load_cond = function(self) return Config:GetSetting('UseFading') and Casting.CanUseAA('Fading Memories') end,
                 cond = function(self, aaName)
-                    return self.Helpers.UnwantedAggroCheck(self)
+                    return mq.TLO.Me.PctHPs() <= Config:GetSetting('EmergencyStart') and self.Helpers.UnwantedAggroCheck(self)
                     --I wanted to use XTAggroCount here but it doesn't include your current target in the number it returns and I don't see a good workaround. For Loop it is.
                 end,
             },
